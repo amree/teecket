c7fe44f3ed82390a2eb54aa05a2d74d0474743c4
amree <mohd.amree@gmail.com>
2015-06-11 04:52:42 +0800

    Align fares to the right, better for money's type data

--- lib/teecket/printer.rb
+++ lib/teecket/printer.rb
@@ -1,8 +1,11 @@
 require 'terminal-table'
+require 'byebug'
 
 class Printer
   def self.table(rows = [])
     headings = ['Flight', 'Flight #', 'Origin', 'Destination', 'Depart', 'Arrive', 'Fare (RM)']
+
+    rows.each { |row| row[:fare] = row[:fare].rjust(9, ' ') }
 
     new_rows = rows.map { |row| row.values }
 
