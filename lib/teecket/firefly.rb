1ea2fe7a0b4b9ea18d3ca8347bad7a52cadf25e7
amree <mohd.amree@gmail.com>
2015-06-10 13:50:00 +0800

    Add initial scrapper for Firefly

---
+++ lib/teecket/firefly.rb
@@ -0,0 +1,73 @@
+require 'nokogiri'
+
+class Firefly < Flight
+  def get
+
+    uri = URI('https://m.fireflyz.com.my/')
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    req = Net::HTTP::Get.new(uri.path)
+
+    res = http.request(req)
+
+    cookie = res['Set-Cookie']
+
+    # byebug
+
+    uri = URI('https://m.fireflyz.com.my/Search')
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    req = Net::HTTP::Get.new(uri.path, initheader = { 'Cookie' => cookie})
+
+    cookie = res['Set-Cookie']
+
+    # byebug
+
+    uri = URI('https://m.fireflyz.com.my/Search')
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    req = Net::HTTP::Post.new(uri.path, initheader = { 'Cookie' => cookie})
+    req.body = 'type=2&departure_station=KBR&arrival_station=SZB&departure_date=30%2F06%2F2015&return_date=30%2F06%2F2015&adult=1&infant=0'
+
+    res = http.request(req)
+
+    # byebug
+
+    if res['location']
+      uri = URI(res['location'])
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      req = Net::HTTP::Get.new(uri.path, initheader = { 'Cookie' => cookie})
+
+      res = http.request(req)
+
+      doc = Nokogiri::HTML(res.body)
+
+      doc.css('div.market1').each_with_index do |elem, i|
+        depart_at     = doc.css('div.market1')[i].css('div.visible-xs').css('table')[1].css('td')[0].text.strip
+        arrive_at     = doc.css('div.market1')[i].css('div.visible-xs').css('table')[1].css('td')[1].text.strip
+        fare          = doc.css('div.market1')[i].css('div.visible-xs > div').text.strip
+        flight_number = doc.css('div.market1')[i].css('div.visible-xs').css('table')[0].text.strip
+
+        depart_at     = DateTime.parse("#{date} #{depart_at.gsub(/\t/, '').match(/^(.*?)(AM|PM)/).to_s}").strftime('%I:%M %p')
+        arrive_at     = DateTime.parse("#{date} #{arrive_at.gsub(/\t/, '').match(/^(.*?)(AM|PM)/).to_s}").strftime('%I:%M %p')
+        fare          = fare.gsub(/ MYR/, '')
+        flight_number = flight_number.gsub(/ /, '').gsub(/FLIGHTNO\./, '')
+
+        fares << [ 'Firefly', flight_number, depart_at, arrive_at, fare ]
+      end
+    end
+  end
+end
