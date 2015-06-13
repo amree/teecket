b29ed4e6f050b28f61455437e93fcf71b8f71e97
amree <mohd.amree@gmail.com>
2015-06-13 15:43:33 +0800

    Initial major refactor for Firefly

--- lib/teecket/firefly.rb
+++ lib/teecket/firefly.rb
@@ -3,15 +3,32 @@ require "nokogiri"
 class Firefly < Flight
   include PageRequester
 
+  attr_accessor :res, :cookie
+
+  def search
+    get
+    process
+  end
+
+  private
+
   def get
+    get_main_page
+    get_search_page
+    get_result_page
+  end
+
+  def get_main_page
     uri = URI("https://m.fireflyz.com.my/")
 
     req = Net::HTTP::Get.new(uri.path)
 
-    res = request(uri, req)
+    self.res = request(uri, req)
 
-    cookie = res["Set-Cookie"]
+    self.cookie = res["Set-Cookie"]
+  end
 
+  def get_search_page
     uri = URI("https://m.fireflyz.com.my/Search")
 
     req = Net::HTTP::Post.new(uri.path, "Cookie" => cookie)
@@ -25,75 +42,100 @@ class Firefly < Flight
       ["departure_date", date]
     ])
 
-    res = request(uri, req)
+    self.res = request(uri, req)
+  end
 
+  def get_result_page
     if res["location"]
       uri = URI(res["location"])
 
       req = Net::HTTP::Get.new(uri.path, "Cookie" => cookie)
 
-      res = request(uri, req)
+      self.res = request(uri, req)
+    end
+  end
 
-      doc = Nokogiri::HTML(res.body)
+  def process
+    doc = Nokogiri::HTML(res.body)
 
-      doc.css("div.market1").each_with_index do |_elem, i|
-        depart_at = doc.css("div.market1")[i]
-                    .css("div.visible-xs")
-                    .css("table")[1]
-                    .css("td")[0]
-                    .text.strip
+    doc.css("div.market1").each do |elem|
+      depart_at = depart_at_selector(elem)
+      arrive_at = arrive_at_selector(elem)
+      fare = fare_selector(elem)
+      flight_number = flight_number_selector(elem)
+      origin = origin_selector(elem)
+      destination = destination_selector(elem)
+      transit = "NO"
 
-        arrive_at = doc
-                    .css("div.market1")[i]
-                    .css("div.visible-xs")
-                    .css("table")[1]
-                    .css("td")[1]
-                    .text.strip
-
-        fare = doc
-               .css("div.market1")[i]
-               .css("div.visible-xs > div")
-               .text.strip
-
-        flight_number = doc
-                        .css("div.market1")[i]
-                        .css("div.visible-xs")
-                        .css("table")[0]
-                        .text.strip
-
-        origin = doc
-                 .css("div.market1")[i]["onclick"]
-                 .scan(/~[A-Z]{3}~/)[0]
-                 .gsub("~", "")
-
-        destination = doc
-                      .css("div.market1")[i]["onclick"]
-                      .scan(/~[A-Z]{3}~/)[1]
-                      .gsub("~", "")
-
-        transit = "NO"
-
-        depart_at = DateTime
-                    .parse("#{date} #{depart_at.gsub(/\t/, '')
-                    .match(/^(.*?)(AM|PM)/)}")
-                    .strftime("%I:%M %p")
-
-        arrive_at = DateTime.parse("#{date} #{arrive_at.gsub(/\t/, '')
-                    .match(/^(.*?)(AM|PM)/)}")
-                    .strftime("%I:%M %p")
-
-        fare = fare.gsub(/ MYR/, "")
-        flight_number = flight_number.gsub(/ /, "").gsub(/FLIGHTNO\./, "")
-
-        fares << { flight_name: "Firefly",
+      add_to_fares(flight_name: "Firefly",
                    flight_number: flight_number,
                    transit: transit,
                    origin: origin,
                    destination: destination,
                    depart_at: depart_at,
                    arrive_at: arrive_at,
-                   fare: fare }
-      end
+                   fare: fare)
+    end
+  end
+
+  def depart_at_selector(elem)
+    value = elem
+            .css("div.visible-xs")
+            .css("table")[1]
+            .css("td")[0]
+            .text.strip
+
+    value_formatter("depart_at", value)
+  end
+
+  def arrive_at_selector(elem)
+    value = elem
+            .css("div.visible-xs")
+            .css("table")[1]
+            .css("td")[1]
+            .text.strip
+
+    value_formatter("arrive_at", value)
+  end
+
+  def fare_selector(elem)
+    value = elem
+            .css("div.visible-xs > div")
+            .text.strip
+
+    value_formatter("fare", value)
+  end
+
+  def flight_number_selector(elem)
+    value = elem
+            .css("div.visible-xs")
+            .css("table")[0]
+            .text.strip
+
+    value_formatter("flight_number", value)
+  end
+
+  def origin_selector(elem)
+    elem["onclick"]
+      .scan(/~[A-Z]{3}~/)[0]
+      .gsub("~", "")
+  end
+
+  def destination_selector(elem)
+    elem["onclick"]
+      .scan(/~[A-Z]{3}~/)[1]
+      .gsub("~", "")
+  end
+
+  def value_formatter(type, value)
+    case type
+    when "depart_at", "arrive_at"
+      value.gsub!(/\t/, "").match(/^(.*?)(AM|PM)/)[0]
+      DateTime.parse("#{date} #{value}").strftime("%I:%M %p")
+    when "fare"
+      value.gsub(/ MYR/, "")
+    when "flight_number"
+      value.gsub(/ /, "").gsub(/FLIGHTNO\./, "")
     end
   end
 end
--- lib/teecket/flight.rb
+++ lib/teecket/flight.rb
@@ -12,4 +12,21 @@ class Flight
 
     @fares = []
   end
+
+  def search
+    raise NotImplementedError
+  end
+
+  private
+
+  def add_to_fares(params)
+    fares << { flight_name: params[:flight_name],
+               flight_number: params[:flight_number],
+               transit: params[:transit],
+               origin: params[:origin],
+               destination: params[:destination],
+               depart_at: params[:depart_at],
+               arrive_at: params[:arrive_at],
+               fare: params[:fare] }
+  end
 end
