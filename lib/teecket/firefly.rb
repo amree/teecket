7e3a3416e9d90bfa49508da442504421b817b114
amree <mohd.amree@gmail.com>
2015-08-16 12:19:04 +0800

    Adapt to FireFly changes

--- lib/teecket/firefly.rb
+++ lib/teecket/firefly.rb
@@ -21,52 +21,66 @@ class Firefly < Flight
   end
 
   def get_main_page
-    uri = URI("https://m.fireflyz.com.my/")
+    uri = URI("http://fireflymobile.me-tech.com.my/fylive3/search.php")
 
     req = Net::HTTP::Get.new(uri.path)
 
-    self.res = request(uri, req)
+    self.res = request(uri, req, false)
 
     self.cookie = res["Set-Cookie"]
   end
 
   def get_search_page
-    uri = URI("https://m.fireflyz.com.my/Search")
+    uri = URI("http://fireflymobile.me-tech.com.my/fylive3/search.php")
+
+    date_object = Date.strptime(date, "%d-%m-%Y")
+    date_year = date_object.strftime("%Y")
+    date_month = date_object.strftime("%m")
+    date_day = date_object.strftime("%d")
 
     req = Net::HTTP::Post.new(uri.path, "Cookie" => cookie)
     req.body = URI.encode_www_form([
+      ["action", "search"],
       ["type", 2],
-      ["return_date", date],
+      ["departing", from],
+      ["arriving", to],
+      ["d10", date_day],
+      ["d11", date_month],
+      ["d12", date_year],
+      ["departuredate", date],
+      ["d20", date_day],
+      ["d21", date_month],
+      ["d22", date_year],
+      ["returndate", date],
       ["adult", 1],
-      ["infant", 0],
-      ["departure_station", from],
-      ["arrival_station", to],
-      ["departure_date", date]
+      ["infant", 0]
     ])
 
-    self.res = request(uri, req)
+    self.res = request(uri, req, false)
   end
 
   def get_result_page
     if res["location"]
-      uri = URI(res["location"])
+      uri = URI("http://fireflymobile.me-tech.com.my/fylive3/" + res["location"])
 
       req = Net::HTTP::Get.new(uri.path, "Cookie" => cookie)
 
-      self.res = request(uri, req)
+      self.res = request(uri, req, false)
     end
   end
 
   def process
     html = Nokogiri::HTML(res.body)
 
-    flights(html).each do |flight|
+    origin, destination = origin_destination_selector(html)
+
+    flights(html).each_with_index do |flight, i|
+      next if i == 0
+
       depart_at = depart_at_selector(flight)
       arrive_at = arrive_at_selector(flight)
       fare = fare_selector(flight)
       flight_number = flight_number_selector(flight)
-      origin = origin_selector(flight)
-      destination = destination_selector(flight)
       transit = "NO"
 
       add_to_fares(flight_name: "Firefly",
@@ -81,6 +95,6 @@ class Firefly < Flight
   end
 
   def flights(data)
-    data.css("div.market1")
+    data.css("form table:last tr")
   end
 end
--- lib/teecket/selectors/firefly.rb
+++ lib/teecket/selectors/firefly.rb
@@ -2,60 +2,42 @@ module Selectors
   module Firefly
     def depart_at_selector(flight)
       value = flight
-              .css("div.visible-xs")
-              .css("table")[1]
-              .css("td")[0]
+              .css("td")[1]
+              .css("div")[0]
 
-      depart_arrive_at_formatter(value)
+      time_formatter(value)
     end
 
     def arrive_at_selector(flight)
       value = flight
-              .css("div.visible-xs")
-              .css("table")[1]
               .css("td")[1]
+              .css("div")[1]
 
-      depart_arrive_at_formatter(value)
+      time_formatter(value)
     end
 
     def fare_selector(flight)
       value = flight
-              .css("div.visible-xs > div")
-
-      fare_formatter(value)
+              .css("td")[2]
+              .css("div")[1]
+              .text
     end
 
     def flight_number_selector(flight)
       value = flight
-              .css("div.visible-xs")
-              .css("table")[0]
-
-      flight_number_formatter(value)
+              .css("td")[0]
+              .text
     end
 
-    def origin_selector(flight)
-      flight["onclick"]
-        .scan(/~[A-Z]{3}~/)[0]
-        .gsub("~", "")
+    def origin_destination_selector(html)
+      text = html.css('form > div')[1].text
+      text.scan(/[A-Z]{3}/)
     end
 
-    def destination_selector(flight)
-      flight["onclick"]
-        .scan(/~[A-Z]{3}~/)[1]
-        .gsub("~", "")
-    end
 
-    def depart_arrive_at_formatter(datetime)
-      datetime = datetime.text.strip.gsub(/\t/, "").match(/^(.*?)(AM|PM)/)[0]
-      DateTime.parse("#{date} #{datetime}").strftime("%I:%M %p")
-    end
-
-    def fare_formatter(fare)
-      fare.text.strip.gsub(/ MYR/, "")
-    end
-
-    def flight_number_formatter(flight_number)
-      flight_number.text.strip.gsub(/ /, "").gsub(/FLIGHTNO\./, "")
+    def time_formatter(element)
+      DateTime.strptime(element.text, "%l:%M%p")
+              .strftime("%I:%M %p")
     end
   end
 end
